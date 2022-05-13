3e31f750f4989f08224789eec981a9635293b812
djdt <10266332+djdt@users.noreply.github.com>
2022-05-13 14:31:12 +1000

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="extma",
-    version="0.1.1",
+    version="0.1.2",
     description="Extractor for microarray or spotted tissue LA-ICP-MS data.",
     packages=["extma"],
     license="LGPL",
